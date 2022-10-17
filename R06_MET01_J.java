c44ff71bbb796e981c29410662b5f7c3f231ec94
rasuar42 <rafaelsu10@hotmail.com>
2022-10-17 12:35:21 -0400

    Security Impementations

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -2,21 +2,17 @@
 //MET01-J. Never use assertions to validate method arguments Given the non-compliant code below:
 
 
-//Noncompliant Code Example
-//The method getAbsAdd() computes and returns the sum of the absolute value of parameters x and y. It lacks argument validation, in violation of MET00-J. Validate method arguments. Consequently, it can produce incorrect results because of integer overflow or when either or both of its arguments are Integer.MIN_VALUE.
-public static int getAbsAdd(int x, int y) {
-  return Math.abs(x) + Math.abs(y);
-}
-getAbsAdd(Integer.MIN_VALUE, 1);
+//Compliant Solution
+//This compliant solution validates the method arguments by ensuring that values passed to Math.abs() exclude Integer.MIN_VALUE and also by checking for integer overflow:
 
-
-//Noncompliant Code Example
-//This noncompliant code example uses assertions to validate arguments of a public method:
 public static int getAbsAdd(int x, int y) {
-  assert x != Integer.MIN_VALUE;
-  assert y != Integer.MIN_VALUE;
+  if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
+    throw new IllegalArgumentException();
+  }
   int absX = Math.abs(x);
   int absY = Math.abs(y);
-  assert (absX <= Integer.MAX_VALUE - absY);
+  if (absX > Integer.MAX_VALUE - absY) {
+    throw new IllegalArgumentException();
+  }
   return absX + absY;
 }
--- R13_FIO01_J.java
+++ R13_FIO01_J.java
@@ -1,7 +1,25 @@
 //Rule 13. Input Output (FIO)
 //FIO01-J. Create files with appropriate access permissions
 
-//Noncompliant Code Example
-//The constructors for FileOutputStream and FileWriter do not allow the programmer to explicitly specify file access permissions. In this noncompliant code example, the access permissions of any file created are implementation-defined and may not prevent unauthorized access:
+//Compliant Solution (POSIX)
+//The  I/O facility java.nio provides classes for managing file access permissions. Additionally, many of the methods and constructors that create files accept an argument allowing the program to specify the initial file permissions.
 
-Writer out = new FileWriter("file");
+//The Files.newByteChannel() method allows a file to be created with specific permissions. This method is platform-independent, but the actual permissions are platform-specific. This compliant solution defines sufficiently restrictive permissions for POSIX platforms:
+
+Path file = new File("file").toPath();
+ 
+// Throw exception rather than overwrite existing file
+Set<OpenOption> options = new HashSet<OpenOption>();
+options.add(StandardOpenOption.CREATE_NEW);
+options.add(StandardOpenOption.APPEND);
+ 
+// File permissions should be such that only user may read/write file
+Set<PosixFilePermission> perms =
+    PosixFilePermissions.fromString("rw-------");
+FileAttribute<Set<PosixFilePermission>> attr =
+    PosixFilePermissions.asFileAttribute(perms);
+ 
+try (SeekableByteChannel sbc =
+         Files.newByteChannel(file, options, attr)) {
+  // Write data
+};
